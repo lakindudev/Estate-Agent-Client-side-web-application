5ada7c2c78012266256fe683b5ddb5ccddeafcee
Lakindu <lakinduperera458@gmail.com>
2024-12-24 17:49:08 +0530

    implement the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,12 +2,24 @@ function Footer(){
 
     return(
         <>
-        <Footer className="footer">
+        <footer className="footer">
 
             <ul className="social-media">
-                <li><a></a></li>
+                <li className="instagram"><a href="#" className="link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg></a></li>
+                <li className="linkdin"><a href="#"className="link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg></a></li>
+                <li className="facebook"><a href="#" className="link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg></a></li>
+                <li className="gmail"><a href="#" className="link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg></a></li>
             </ul>
-        </Footer>
+
+            <ul className="nav-footer">
+                <li className="footer-home"><a href="#" className="nav">Home</a></li>
+                <li className="footer-News"><a href="#" className="nav">News</a></li>
+                <li className="footer-About"><a href="#" className="nav">About</a></li>
+                <li className="footer-Contact"><a href="#" className="nav">Contact Us</a></li>
+            </ul>
+            <hr></hr>
+            <p className="footer-para">&copy; 2024 HavenHub. All rights reserved.</p>
+        </footer>
         </>
     )
 
