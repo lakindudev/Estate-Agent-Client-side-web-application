0b8c17dd75e8fe23e0f1d787e91b1ed7e92726ff
Lakindu <lakinduperera458@gmail.com>
2024-12-24 16:37:34 +0530

    adding logo and styles for the nav-bar

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer(){
+
+    return(
+        <>
+        <Footer className="footer">
+
+            <ul className="social-media">
+                <li><a></a></li>
+            </ul>
+        </Footer>
+        </>
+    )
+
+}
+export default Footer;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,22 @@
-function NavBar(){
+import logo from '../images/logo.png'; 
 
-    return(
-        
-       <>
-         <nav>
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/search">Search</a></li>
-                <li><a href="/favorites">Favorites</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-            </ul>
-        </nav>
-       </>
+function NavBar() {
+    return (
+        <>
+            <nav className='nav-bar'>
+                <div className='logo'>
+                    <img src={logo} alt="Logo" className='logo-img' /> 
+                    <p className='logo-name'>HavenHub</p>
+                </div>
+                <ul className="nav-links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/search">Search</a></li>
+                    <li><a href="/favorites">Favorites</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
+            </nav>
+        </>
     );
 }
 export default NavBar;
