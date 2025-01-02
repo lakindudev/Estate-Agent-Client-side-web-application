2490abc865f6765b7793c3eeeda905853578f375
Lakindu <lakinduperera458@gmail.com>
2025-01-03 01:23:28 +0530

    modify the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ import FavoriteList from "./Components/FavouriteList.jsx";
 import Contact from "./Components/Contact.jsx";
 import About from "./Components/About.jsx";
 import propertiesData from "./properties.json";
-import SearchPage from "./Components/Search.jsx"; // Import the new SearchPage component
+import SearchPage from "./Components/Search.jsx"; 
 
 
 
 function App() {
   const [filteredProperties, setFilteredProperties] = useState(propertiesData.properties);
   const [favorites, setFavorites] = useState([]);
+  
 
   const handleSearch = (formData) => {
     const filtered = propertiesData.properties.filter((property) => {
@@ -43,15 +44,29 @@ function App() {
     });
   };
 
+  // Function to handle click on "Search Now" button
+const handleHomeBtnClick = () => {
+  const searchArea = document.getElementById("search-area");
+  if (searchArea) {
+    searchArea.scrollIntoView({ behavior: "smooth" }); // Smooth scroll to the search area
+  }
+};
+
   return (
     <Router>
       <NavBar />
+      <div className="image-container">
+        <img src="https://t3.ftcdn.net/jpg/03/22/06/68/360_F_322066808_CANrp7u5Cdiz7700TJReqKD299d2AZtD.jpg" alt="landpage_img" className="landpage_img"/>
+        <div className="overlay-heading">Welcome to the HeavenHub</div> 
+        <div className="overlay-text"> Discover your dream Property</div>
+        <button className="home-btn" onClick={handleHomeBtnClick}> Search Now</button>
+      </div>
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <h1 className="search-head">Estate Agent Property Search</h1>
+              <h1 className="search-head" id="search-area">Estate Agent Property Search</h1>
               <SearchForm onSearch={handleSearch} />
               <div className="property-results">
                 {filteredProperties.length > 0 ? (
